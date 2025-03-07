5945391df8faa16c2d4ce681a80bbea79c4c4f1d
sh.anvarov <sanedidi98@gmail.com>
2025-03-07 15:28:13 +0500

    fix;

--- src/pages/layout.js
+++ src/pages/layout.js
@@ -1,13 +1,7 @@
-import { Provider } from "@/components/ui/provider";
-import '@/styles/globals.scss';
-
 export default function RootLayout({ children }) {
-  const { children } = props;
   return (
     <html suppressHydrationWarning>
-      <body>
-        <Provider>{children}</Provider>
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
